912fe4853ab6b6f709dd52f1cfb2f81d2e073e2a
shubh <shubhamb1810@gmail.com>
2019-12-29 20:04:59 -0500

    renamed variables

--- win10/Cord.py
+++ win10/Cord.py
@@ -3,38 +3,37 @@ from win32gui import FindWindow, GetWindowRect
 
 
 class Cord(QObject):
-    __instance = None
-    sig = Signal()
+    __main_instance = None
+    changed = Signal()
 
     #
     @staticmethod
     def get_instance(*args):
-        if Cord.__instance is None:
-            Cord.__instance = Cord()
+        if Cord.__main_instance is None:
+            Cord.__main_instance = Cord()
             print("new")
 
-        return Cord.__instance
+        return Cord.__main_instance
 
     # TODO: find the right window
-    def __init__(self, parent=None, window=FindWindow(None, "cmd")):
-        super().__init__(parent)
-
+    def __init__(self, parent=None, window=FindWindow(None, "File Explorer")):
+        QObject.__init__(self)
         self.__window = window
         self._x_l = self._y_t = self._x_r = self._y_b = 0
 
-    @Property(int, notify=sig)
+    @Property(int, notify=changed)
     def x_l(self):
         return self._x_l + 8
 
-    @Property(int, notify=sig)
+    @Property(int, notify=changed)
     def y_t(self):
         return self._y_t
 
-    @Property(int, notify=sig)
+    @Property(int, notify=changed)
     def x_r(self):
         return self._x_r - 8
 
-    @Property(int, notify=sig)
+    @Property(int, notify=changed)
     def y_b(self):
         return self._y_b
 
@@ -51,4 +50,4 @@ class Cord(QObject):
         if not self.equals(x_l, y_t, x_r, y_b):
             print(x_l, y_t, x_r, y_b)
             self._x_l, self._y_t, self._x_r, self._y_b = x_l, y_t, x_r, y_b
-            self.sig.emit()
+            self.changed.emit()
