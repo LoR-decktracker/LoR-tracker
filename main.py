e8665c80f0795aec505abdce7b7664b817bc363c
shubh <shubhamb1810@gmail.com>
2019-12-31 04:28:35 -0500

    optimized import

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from functools import partial
 
-import pywintypes
+from pywintypes import error
 from qtpy.QtCore import QUrl, QTimer
 from qtpy.QtGui import QGuiApplication
 from qtpy.QtQml import QQmlApplicationEngine, qmlRegisterType
@@ -22,7 +22,7 @@ else:
 def hooking_fun(cord: Cord):
     try:
         cord.hooking()
-    except pywintypes.error:
+    except error:
         # Handel exit
         app.exit()
 
