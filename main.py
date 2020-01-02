772a9dd8bd1853d431a3acd6ead973482b6909c3
shubh <shubhamb1810@gmail.com>
2020-01-01 20:14:09 -0500

    modularized main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import sys
-from functools import partial
 
-from pywintypes import error
 from qtpy.QtCore import QUrl, QTimer
 from qtpy.QtGui import QGuiApplication
 from qtpy.QtQml import QQmlApplicationEngine, qmlRegisterType, qmlRegisterSingletonType
@@ -11,45 +9,27 @@ from dataObjects.BarList import BarList
 
 import platform
 
-# import system specific files
-if platform.system() == "Windows":
-    print("Windows Detected")
-    from win10.Cord import Cord
-else:
-    exit()
+from utility.Setup import Setup
 
 
-def hooking_fun(cord: Cord):
-    try:
-        cord.hooking()
-    except error:
-        # Handel exit
-        app.exit()
-
-# def tr():
-#     return Cord.get_instance
-
-if __name__ == '__main__':
-
+def main():
     app = QGuiApplication([])
     engine = QQmlApplicationEngine()
 
-    # Send Singleton with coordinates of target window to QML
-    c = Cord()
-    qmlRegisterSingletonType(Cord, "Coordinates", 1, 0, "Cord", lambda *e: c)
+    setup = Setup(app, engine)
 
-    # engine.rootContext().setContextProperty("Cord", c)
-    timer = QTimer()
-    timer.timeout.connect(partial(hooking_fun, c))
-    timer.start()
+    setup.setup_hooking()
+    setup.setup_bars()
 
     # FIXME attach to real views
-    lst = [Bar("user1", 1, 3, Region.DEMACIA), Bar("User2", 2, 4, Region.DEMACIA)]
-    barList = BarList(lst)
-    engine.rootContext().setContextProperty("lst", barList)
+
 
     # Load QML
     url = QUrl("qml/main.qml")
     engine.load(url)
 
     sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
---
+++ utility/Setup.py
@@ -0,0 +1,47 @@
+import platform
+from functools import partial
+
+from qtpy.QtCore import QTimer
+from qtpy.QtGui import QGuiApplication
+from qtpy.QtQml import qmlRegisterSingletonType, QQmlApplicationEngine
+
+from pywintypes import error
+
+# import system specific files
+from dataObjects.Bar import Bar, Region
+from dataObjects.BarList import BarList
+
+if platform.system() == "Windows":
+    print("Windows Detected")
+    from win10.Cord import Cord
+else:
+    exit()
+
+
+class Setup:
+    def __init__(self, app: QGuiApplication, engine: QQmlApplicationEngine):
+        self.app = app
+        self.engine = engine
+
+        self.timer = QTimer()
+        self.c = Cord()
+
+    def setup_hooking(self):
+        # Send Singleton with coordinates of target window to QML
+        qmlRegisterSingletonType(Cord, "Coordinates", 1, 0, "Cord", lambda *args: self.c)
+        # engine.rootContext().setContextProperty("Cord", c)
+
+        self.timer.timeout.connect(partial(self._hooking_fun, self.c))
+        self.timer.start()
+
+    def _hooking_fun(self, cord: Cord):
+        try:
+            cord.hooking()
+        except error:
+            # Handel exit
+            self.app.exit()
+
+    def setup_bars(self):
+        lst = [Bar("user1", 1, 3, Region.DEMACIA), Bar("User2", 2, 4, Region.DEMACIA)]
+        self.barList = BarList(lst)
+        self.engine.rootContext().setContextProperty("lst", self.barList)
